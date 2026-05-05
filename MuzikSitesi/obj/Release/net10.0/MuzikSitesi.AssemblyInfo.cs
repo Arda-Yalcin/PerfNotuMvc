8a881273cb9455f0fd23b3841425f5c98d5be7ea
Arda Yalçın <yalcinarda734@gmail.com>
2026-05-05 13:41:04 +0300

    .

--- MuzikSitesi/obj/Release/net10.0/MuzikSitesi.AssemblyInfo.cs
+++ MuzikSitesi/obj/Release/net10.0/MuzikSitesi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzikSitesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d8ee3c2b546883f903c51a6413d9c97febbfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb5f407114d3e0f37ce7eac27ad86d19553825ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzikSitesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzikSitesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
